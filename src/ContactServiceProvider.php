4f160632a941353bbb803004fb5bfde58e8f6cba
tekle yitayew <codeethiopia@gmail.com>
2022-02-17 13:06:50 -0800

    publish contact migrations

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -13,6 +13,11 @@ class ContactServiceProvider extends ServiceProvider{
     $this->publishes([
         __DIR__.'/config/contact.php' => config_path('contact.php'),
         __DIR__.'/views/' => resource_path('views/vendor/contact'), 
+       
+        
+        $this->publishes([
+          __DIR__.'/database/migrations/' => database_path('migrations')
+      ], 'contact-migrations')
     ]);
   }
   public function register(){
